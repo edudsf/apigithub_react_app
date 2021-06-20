99ce4eb46c428a13d87428299a0a69465cbe2258
EDUDSF <eduardo.sf.1992@gmail.com>
2021-06-20 03:20:00 -0300

    feat: add new routes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,3 @@
 export { default as Search } from './Search'
+export { default as Home } from './Home'
+export { default as Repositories } from './Repositories'
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Search } from '@/pages'
+import { Search, Home, Repositories } from '@/pages'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Search} />
+      <Route exact path='/home' component={Home} />
+      <Route exact path='/repositories' component={Repositories} />
     </Switch>
   </BrowserRouter>
 )
